27428cea242a9787f3c764165dc797f089d38853
Holland Smith <railsholland@gmail.com>
2011-05-25 14:37:18 -0700

    Undid Ajax functionality for now to push to Heroku

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,22 +1,26 @@
 class RelationshipsController < ApplicationController
 
 	before_filter :authenticate
+	
+#see pg. 501 - couldnt get ajay to work yet
 
 	def create
 		@user = User.find(params[:relationship][:followed_id])
 		current_user.follow!(@user)
-		respond_to do |format|
-			format.html { redirect_to @user }
-			format.js
-		end
+		redirect_to @user
+		#respond_to do |format|
+		#	format.html { redirect_to @user }
+		#	format.js
+		#end
 	end
 
 	def destroy
 		@user = Relationship.find(params[:id]).followed
 		current_user.unfollow!(@user)
-		respond_to do |format|
-			format.html { redirect_to @user }
-			format.js
-		end
+		redirect_to @user
+		#respond_to do |format|
+		#	format.html { redirect_to @user }
+		#	format.js
+		#end
 	end
 end	
